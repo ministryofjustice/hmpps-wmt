1da34f6419427b42a2ff40ad96cd9fe6edd4e821
Tom Swann <ts.swann@gmail.com>
2017-03-01 16:57:02 +0000

    Updated ETL script to use Pandas and SQL Alchemy  - Testing Schema inference and automatic staging table creation

--- wmt-etl/etl.py
+++ wmt-etl/etl.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
+db = create_engine('postgresql://wmt:wmt@192.168.99.100/wmt_db')
 WORKBOOK_FILEPATH = './data/WMT_Extract_sample.xlsx'
 SUMMARY_SHEET = 'WMT_Extract'
 COURT_REP_SHEET = 'Court_Reports'
@@ -9,5 +11,5 @@ WORKBOOK = pd.ExcelFile(WORKBOOK_FILEPATH)
 dataframes = {}
 for sheet in WORKBOOK.sheet_names:
     dataframes[sheet] = WORKBOOK.parse(sheet)
-
-
+    df = dataframes[sheet]
+    df.to_sql('stg_' + sheet.lower(), db, if_exists='replace')
