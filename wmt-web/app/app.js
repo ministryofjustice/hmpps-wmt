a90329117ce2de2e581645e506e57fe59d6ba4de
Andrew Wright <andrew.js.wright@gmail.com>
2017-03-01 14:36:34 +0000

    Add plotly integration (#3)
    * Create data sevice for caseload-utilisation access
    
    * Add plotly integration
    
    Store the data to be plotted in a script tag at the end of the body and parse it
    in the graph's javascript file. This is an alternative to making an HTTP request
    on page load to retrieve the data.

--- wmt-web/app/app.js
+++ wmt-web/app/app.js
@@ -1,4 +1,3 @@
-const config = require('../config')
 const express = require('express')
 const nunjucks = require('express-nunjucks')
 const favicon = require('serve-favicon')
---
+++ wmt-web/app/routes/caseload-utilisation.js
@@ -0,0 +1,14 @@
+const getCaseloadUtilisation = require('../services/data/get-caseload-utilisation')
+module.exports = function (router) {
+    router.get('/my-utilisation/', function (req, res, next) {
+        // When the user session stories are played the user id will be attached
+        // to the req or res object
+        var myUserId = 1;
+        var year = 2016;
+        getCaseloadUtilisation(myUserId, year).then(function (utilisations) {
+            return res.render('my-utilisation', {
+                utilisations: utilisations
+            })
+        })
+    })
+}
