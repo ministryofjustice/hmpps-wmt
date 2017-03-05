c442d1632a0b4711438b003ddf48375de9bdccaa
Tom Swann <ts.swann@gmail.com>
2017-03-05 19:37:30 +0000

    Added Standard JS support and fixed all format errors

--- wmt-web/app/services/data/get-caseload-utilisation.js
+++ wmt-web/app/services/data/get-caseload-utilisation.js
@@ -2,16 +2,16 @@ const config = require('../../../knexfile').development
 const knex = require('knex')(config)
 
 module.exports = function (userId, year) {
-    return knex('caseload_utilisation')
+  return knex('caseload_utilisation')
         .select('utilisation_percentage')
         .join('month_lookup', 'month_id', '=', 'month_lookup.id')
-        .where({'user_id': userId, 'year' : year })
+        .where({ 'user_id': userId, 'year': year })
         .orderBy('ordinal_value', 'asc')
-        .then(function(results) {
-            var values = []
-            results.forEach(function(result) {
-                values.push(result["utilisation_percentage"])
-            })
-            return values
+        .then(function (results) {
+          var values = []
+          results.forEach(function (result) {
+            values.push(result['utilisation_percentage'])
+          })
+          return values
         })
 }
